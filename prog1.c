5184c6d701b5922b428978538dd38feaceb33b85
Rachel Keiser <rachelk00@vt.edu>
2023-02-10 16:27:05 -0500

    Getting much more reasonable times for page faults now

--- prog1.c
+++ prog1.c
@@ -9,19 +9,16 @@ static inline uint64_t rdtsc() {
     return ((uint64_t)edx << 32) | eax; 
 }
 
-
- 
 int main() { 
+    char* ptr = malloc(4192);
     uint64_t start = rdtsc(); 
     // code to be timed
-    char* ptr = malloc(4192);
     ptr[0] = 0x8;
     // ************ 
     uint64_t end = rdtsc(); 
     uint64_t elapsed = end - start; 
-    printf("Elapsed time: %lu cycles\n", elapsed);
-    // Convert to seconds
-    double microseconds = elapsed * 1000000 / (float)CLOCKS_PER_SEC;
-    printf("With %lu clocks per second, this equals %f microseconds\n", CLOCKS_PER_SEC, microseconds);
+    // Convert to microseconds
+    int microseconds = elapsed * 1000000 / (float)CLOCKS_PER_SEC;
+    printf("%d seconds\n", CLOCKS_PER_SEC, microseconds);
     return 0; 
 } 
